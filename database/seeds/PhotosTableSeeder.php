062d09daed8cee3c5375b62336a2fe97d5179eaa
luca <luca.carnaroli98@gmail.com>
2020-03-26 17:28:57 +0100

    user

---
+++ database/seeds/PhotosTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Photo;
+
+
+class PhotosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 10; $i++) { 
+            $newPhoto = new Photo;
+            $random = rand(1,100);
+            $newPhoto->name = $faker->name;
+            $newPhoto->img = "https://picsum.photos/id/$random/300/200";
+            $newPhoto->user_id = rand(1,5);
+            $newPhoto->save();
+        }
+    }
+}
